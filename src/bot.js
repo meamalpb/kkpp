824ce54d2a7090f7ad85e96eb1e9acd502ce2bdf
ben-jnr <bichubenkuruvilla@gmail.com>
2021-05-17 02:15:27 +0530

    new pull

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+require('discord-buttons')(client);
 
 
 client.on("ready",()=>{
@@ -8,12 +9,26 @@ client.on("ready",()=>{
 });
 
 
-client.on("message",(message)=>{
-    if(message.author.bot)
-        return;
-    console.log(`${message.author.username} : ${message.content}`);
-    client.users.cache.get(message.author.id).send(message.content);
+// client.on("message",(message)=>{
+//     if(message.author.bot)
+//         return;
+//     console.log(`${message.author.username} : ${message.content}`);
+//     client.users.cache.get(message.author.id).send(message.content);
+
+    // message.buttons('Hello World!', {
+    //     buttons: [
+    //         {
+    //             style: 'green',
+    //             label: 'Click to function!',
+    //             id: 'click_to_function'
+    //         }
+    //     ]
+    // })
+
+
 });
+
+
 
 
 client.login(process.env.BOT_TOKEN);
