960770986f4000382730925f7f2743989602e3e5
unknown <321amalpb.com@gmail.com>
2021-05-17 02:06:30 +0530

    menu

--- src/bot.js
+++ src/bot.js
@@ -1,18 +1,38 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '_';
+const axios = require('axios');
 
+const newEmbed = new Discord.MessageEmbed().setTitle("District List").setDescription('\n1.Alappuzha\n2.Ernakulam\n3.Idukki\n4.Kannur\n5.kasargode\n6.Kollam\n7.Kottayam\n8.Kozhikode\n9.Malappuram\n10.Palakkad\n11.Pathanamthitta\n12.Truandrum\n13.Thrissur\n14.Wayanad\nCommand:\n_district 1   |  _district 2  |  _district 3 ...')
 
-client.on("ready",()=>{
+client.on("ready",async()=>{
     console.log(`${client.user.tag} is ready`);
 });
 
 
-client.on("message",(message)=>{
+client.on("message",async(message)=>{
     if(message.author.bot)
         return;
-    console.log(`${message.author.username} : ${message.content}`);
-    client.users.cache.get(message.author.id).send(message.content);
+
+    if(message.content==`${prefix}menu`){
+        axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/districts/17',
+{
+    headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0',
+      },
+},)
+.then((response)=>{
+    const districts=response.data.districts;
+    console.log(districts)
+    return message.channel.send(newEmbed);
+
+})
+        
+    }
+
+
 });
 
 
